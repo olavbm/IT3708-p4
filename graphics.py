cec120acce7a8b0cd21e37c5da03d260cadaa74b
Olav Markussen <olavbm@samfundet.no>
2016-03-09 15:06:49 +0100

    Adding main graphics file. Support for simple window-surface.

---
+++ graphics.py
@@ -0,0 +1,8 @@
+import pygame
+
+windowDimensions = [700, 700]
+windowSurface = pygame.display.set_mode((windowDimensions[0], windowDimensions[1]), 0, 32)
+windowSurface.fill((255, 255, 255))
+
+
+pygame.display.update()
