f73af52ee9ec723816547244b33f78d2f86a5650
Olav Markussen <olavbm@samfundet.no>
2016-03-11 16:17:32 +0100

    Add test for drawing of claus.

--- graphics_test.py
+++ graphics_test.py
@@ -4,7 +4,7 @@ import time
 import pygame
 
 class TestGraphics(unittest.TestCase):
-    def testGraphicsFromMatrix(self):
+    def testSimpleGraphicsFromMatrix(self):
         matrix = [ [0 for i in range(10)] for i in range(10)]
         matrix[3] = ["P" for i in range(10)]
 
@@ -19,7 +19,28 @@ class TestGraphics(unittest.TestCase):
         pygame.display.update()
         graphics.drawBoardFromMatrix(windowSurface, matrix, grid, cellSize)
         pygame.display.update()
-        time.sleep(2)
+        time.sleep(1)
+
+    def testFoodPoisonClauseFromMatrix(self):
+        matrix = [ [0 for i in range(10)] for i in range(10)]
+        matrix[3] = ["P" for i in range(10)]
+
+        for i in range(10):
+            matrix[i][4] = "F"
+
+        windowDimensions = [700, 700]
+        grid = [10, 10]
+        cellSize = [windowDimensions[0]/grid[0], windowDimensions[1]/grid[1]]
+        windowSurface = pygame.display.set_mode((windowDimensions[0], windowDimensions[1]), 0, 32)
+
+        matrix[5][7] = "D"
+        matrix[1][4] = "U"
+        matrix[0][9] = "R"
+        matrix[3][0] = "L"
+
+        graphics.drawBoardFromMatrix(windowSurface, matrix, grid, cellSize)
+        pygame.display.update()
+        time.sleep(1)
 
 
 
