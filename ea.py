4aa9f8289030e18f228ea272b562f285d5435780
Olav Markussen <olavbm@samfundet.no>
2016-03-16 16:36:32 +0100

    Adds some meat to several skeleton-functions.

--- ea.py
+++ ea.py
@@ -1,4 +1,3 @@
-
 class Candidate(object):
     def __init__(self, gType):
         '''
@@ -8,9 +7,11 @@ class Candidate(object):
         self.gType = gType
         self.weights = [[]]
 
-    # Fitness evaluation function.
-    # Somehow needs to pass board to the ann it represents.
-    def calculateFitness(self):
+    '''
+    Fitness evaluation function.  
+    Somehow needs to pass board to the ann for action-response.
+    '''
+    def calculatefitness(self):
         pass
 
     def mutate(self, probability):
@@ -20,15 +21,73 @@ class Candidate(object):
         pass
 
 class Population(object):
-    def __init__(self):
-        pass
+    def __init__(self, candidate, size, maxGenerations):
+        self.candidate = candidate
+        self.size = size
+        self.maxGenerations = maxGenerations
+        self.population = self.generatePopulation(self.size)
 
-    def generateCandidate():
-        pass
+    '''
+    This needs some fixing. How do we initialize the ann, and with what
+    kind of matrix? Should it just take care of that itself? But what with
+    the gType then?
+    '''
+    def initializeCandidate():
+        return self.candidate()
 
-    def generatePopulation(self, size):
-        pass
+    '''
+    Initializes the population.
+    Uses the candidate provided when initializing the population.
+    '''
+    def initializePopulation(self, size):
+        population = []
+        for i in range(size):
+            population.append(self.initializeCandidate())
 
-    # Main evolution loop.
+        return population
+
+    '''
+    Main evolution loop.
+    Decision on what parent-selection-function to be used needs to be done.
+    '''
     def evolve():
+        for i in range(self.maxGenerations):
+            generation = generateGeneration()
+
+    '''
+    Sum all fitnesses up to a number, µ.
+    Pick a number between 0 and µ.
+    For each candidate, sum the fitnesses.
+    If sumFitness > µ, you have your candidate.
+    '''
+    def fitnessProportionateSelection():
         pass
+
+    def generateGeneration():
+        generation = []
+        for i in range(self.size):
+            generation.append(self.reproduction())
+
+        return generation
+
+    def reproduction():
+        rand1 = int(random.random() * self.size -1)
+        rand2 = int(random.random() * self.size -1)
+        parent1 = self.population[rand1]
+        parent2 = self.population[rand2]
+        child = parent1.crossover(parent2)
+        child.mutate()
+
+        return child
+
+def run():
+    # It is possible to have many different candidates here.
+    def exempleCandidate(Candidate):
+        # A specialized mutate-function for this exact problem and candidate.
+        def mutate(probability):
+            pass
+
+    size = 100
+    maxGenerations = 100
+    population = Population(exempleCandidate, size, maxGenerations)
+    result = population.evolve()
