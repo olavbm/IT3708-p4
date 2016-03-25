63e76bcbd16a0e75f65fdb41c98c8bd15e8e0884
Olav Markussen <olavbm@samfundet.no>
2016-03-25 15:09:08 +0100

    Change to a simple 2-layer FF network.

--- neuron.py
+++ neuron.py
@@ -1,13 +1,16 @@
 import numpy as np
+import pprint
 
 iter_limit = 100
 input_count = 6
+hidden_count = 0
+output_count = 3
 
 class Neural_net(object):
     def __init__ (self, weights):
         self.weights = weights
-        hidden_count = weights.shape[0]
-        self.syn = np.zeros(hidden_count)
+        self.hidden_count = weights.shape[0]
+        self.syn = np.ones(self.hidden_count)
 
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
@@ -16,7 +19,9 @@ class Neural_net(object):
         syn = self.syn
         dot = np.dot
         cat = np.concatenate
-        for x in range(iter_limit):
-            syn = sigmoid(dot(weights, cat((inputs, syn))))
+        sigmoid = self.sigmoid
+        syn = sigmoid(dot(inputs, self.weights))
+        for i in range(len(self.weights)):
+            syn = sigmoid(dot(self.weights[i], syn))
         self.syn = syn
-        return syn[:3]
+        return syn
