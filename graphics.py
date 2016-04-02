20bdb0437f104f412b17871a80c98b290111b96d
Olav Markussen <olavmarkussen@hotmail.no>
2016-04-03 01:19:10 +0200

    Add class Painter in graphics.
    Drawing/painting is hopefully easier to do now. Painter class handles
    everything relating to canvas and painting in general.
    Rewrite tests so they use the new class and code-style.
    
    Conflicts:
    	graphics.py
    	graphics_test.py

--- graphics.py
+++ graphics.py
@@ -1,101 +1,99 @@
 import pygame
 from pygame import gfxdraw
 
-windowDimensions = [700, 700]
-grid = [10, 10]
-cellSize = [windowDimensions[0]/grid[0], windowDimensions[1]/grid[1]]
-windowSurface = pygame.display.set_mode((windowDimensions[0], windowDimensions[1]), 0, 32)
+class Painter:
+    def __init__(self):
+        self.window_dimensions = [700, 700]
+        self.grid = [10, 10]
+        self.cell_size = [self.window_dimensions[0]/self.grid[0], self.window_dimensions[1]/self.grid[1]]
+        self.window_surface = pygame.display.set_mode((self.window_dimensions[0], self.window_dimensions[1]), 0, 32)
+        self.gnome_sheet = self.calculate_claus_sprites_from_sheet("clause.png")
+        self.colours = {
+                "black": (0, 0, 0),
+                "white": (255, 255, 255),
+                "green": (0, 255, 0),
+                "grey":  (100, 100, 100),
+                "red":   (255, 0 , 0)
+            }
+        self.offset_vec = {
+                "U": [-25,-26],
+                "L": [-23,-29],
+                "R": [-25,-26],
+                "D": [-25,-28]
+            }
 
-colours = {
-        "black": (0, 0, 0),
-        "white": (255, 255, 255),
-        "green": (0, 255, 0),
-        "grey":  (100, 100, 100),
-        "red":   (255, 0 , 0)
-    }
-offsetVec = {
-        "U": [-25,-26],
-        "L": [-23,-29],
-        "R": [-25,-26],
-        "D": [-25,-28]
-    }
+    # Helper-function that collects individual sprites from a big spritesheet.
+    def calculate_claus_sprites_from_sheet(self, filename):
+        gnome_sheet = {}
+        sheet = pygame.image.load("clause.png")
 
-# Helper-function that collects individual sprites from a big spritesheet.
-def calculateClauseSpritesFromSheet(filename):
-    gnomeSheet = {}
-    sheet = pygame.image.load("clause.png")
+        sheet.set_clip(pygame.Rect(16, 8, 46, 57))
+        gnome_sheet["U"] = sheet.subsurface(sheet.get_clip())
 
-    sheet.set_clip(pygame.Rect(16, 8, 46, 57))
-    upSprite = sheet.subsurface(sheet.get_clip())
-    gnomeSheet["U"] = upSprite
+        sheet.set_clip(pygame.Rect(76, 7, 46, 57))
+        gnome_sheet["R"] = sheet.subsurface(sheet.get_clip())
 
-    sheet.set_clip(pygame.Rect(76, 7, 46, 57))
-    upSprite = sheet.subsurface(sheet.get_clip())
-    gnomeSheet["R"] = upSprite
+        sheet.set_clip(pygame.Rect(24, 132, 46, 57))
+        gnome_sheet["L"] = sheet.subsurface(sheet.get_clip())
 
-    sheet.set_clip(pygame.Rect(24, 132, 46, 57))
-    upSprite = sheet.subsurface(sheet.get_clip())
-    gnomeSheet["L"] = upSprite
+        sheet.set_clip(pygame.Rect(144, 68, 46, 57))
+        gnome_sheet["D"] = sheet.subsurface(sheet.get_clip())
 
-    sheet.set_clip(pygame.Rect(144, 68, 46, 57))
-    upSprite = sheet.subsurface(sheet.get_clip())
-    gnomeSheet["D"] = upSprite
+        return gnome_sheet
 
-    return gnomeSheet
+    # Helper-function that calculates a position in pixels given a matrix-index and cell size.
+    def calculate_pos_from_index(self, matrix_index):
+        return [matrix_index[0] * self.cell_size[0] + self.cell_size[0]/2,
+                matrix_index[1] * self.cell_size[1] + self.cell_size[1]/2]
 
-gnomeSheet = calculateClauseSpritesFromSheet("clause.png")
+    # Draws the entire grid, with background-color.
+    def draw_grid(self):
+        self.window_surface.fill((self.colours["white"]))
+        for x in range(self.grid[0]):
+            pygame.draw.aaline(self.window_surface, self.colours["black"], (self.cell_size[0] * x, 0), (self.cell_size[0] * x, 700))
 
-# Helper-function that calculates a position in pixels given a matrix-index and cell size.
-def calculatePosFromIndex(matrixIndex, cellSize):
-    return [matrixIndex[0] * cellSize[0] + cellSize[0]/2, matrixIndex[1] * cellSize[1] + cellSize[1]/2]
+        for y in range(self.grid[1]):
+            pygame.draw.aaline(self.window_surface, self.colours["black"], (0, self.cell_size[1] * y), (700, self.cell_size[1] * y))
 
-# Draws the entire grid, with background-color.
-def drawGrid(surface, numGrid, cellSize):
-    surface.fill((255, 255, 255))
-    for x in range(numGrid[0]):
-        pygame.draw.aaline(windowSurface, colours["black"], (cellSize[0] * x, 0), (cellSize[0] * x, 700))
+    # Draws a food-sprite in the given cell(ex. [1, 2]) on our surface.
+    def draw_food(self, matrix_index, r):
+        pos = self.calculate_pos_from_index(matrix_index)
+        pygame.gfxdraw.filled_circle(self.window_surface, pos[0], pos[1], r, self.colours["green"])
 
-    for y in range(numGrid[1]):
-        pygame.draw.aaline(windowSurface, colours["black"], (0, cellSize[1] * y), (700, cellSize[1] * y))
+        # Drawing nice anti-aliased circle around the circle.
+        pygame.gfxdraw.aacircle(self.window_surface, pos[0], pos[1], r, self.colours["black"])
 
-# Draws a food-sprite in the given cell(ex. [1, 2]) on the given surface.
-def drawFood(surface, matrixIndex, cellSize, r):
-    pos = calculatePosFromIndex(matrixIndex, cellSize)
-    pygame.gfxdraw.filled_circle(windowSurface, pos[0], pos[1], r, colours["green"])
+    # Draws a poison-sprite in the given cell(ex. [4, 2]) on the given surface.
+    def draw_poison(self, matrix_index, r):
+        pos = self.calculate_pos_from_index(matrix_index)
+        points = [[pos[0], pos[1] - r], [pos[0] + r, pos[1]], [pos[0], pos[1] + r], [pos[0] - r, pos[1]]]
+        pygame.gfxdraw.filled_polygon(self.window_surface, points, self.colours["red"])
 
-    # Drawing nice anti-aliased circle around the circle.
-    pygame.gfxdraw.aacircle(windowSurface, pos[0], pos[1], r, colours["black"])
+        # Drawing nice anti-aliased line around the polygon
+        points = [[pos[0], pos[1] - r], [pos[0] + r, pos[1]], [pos[0], pos[1] + r], [pos[0] - r, pos[1]]]
+        pygame.gfxdraw.aapolygon(self.window_surface, points, self.colours["black"])
 
-# Draws a poison-sprite in the given cell(ex. [4, 2]) on the given surface.
-def drawPoison(surface, matrixIndex, cellSize, r):
-    pos = calculatePosFromIndex(matrixIndex, cellSize)
-    points = [[pos[0], pos[1] - r], [pos[0] + r, pos[1]], [pos[0], pos[1] + r], [pos[0] - r, pos[1]]]
-    pygame.gfxdraw.filled_polygon(windowSurface, points, colours["red"])
+    # Draws a gnome, in a given direction and cell.
+    def draw_gnome(self, matrix_index, direction):
+        pos = self.calculate_pos_from_index(matrix_index)
+        self.window_surface.blit(self.gnome_sheet[direction], [a + b for a, b in zip(pos, self.offset_vec[direction])])
 
-    # Drawing nice anti-aliased line around the polygon
-    points = [[pos[0], pos[1] - r], [pos[0] + r, pos[1]], [pos[0], pos[1]+r], [pos[0]-r, pos[1]]]
-    pygame.gfxdraw.aapolygon(windowSurface, points, colours["black"])
+    # Finds what sprite needs to be drawn, and calls the according function to draw it in place.
+    def draw_rune(self, rune, matrix_index):
+        if rune == 0:
+            return
+        elif rune == "F":
+            self.draw_food(matrix_index, 20)
+        elif rune == "P":
+            self.draw_poison(matrix_index, 20)
+        else:
+            self.draw_gnome(matrix_index, rune)
+        # Add other runes here when needed
 
-# Finds what sprite needs to be drawn, and calls the according function to draw it in place.
-def drawRune(surface, rune, matrixIndex, cellSize):
-    if rune == 0:
-        return
-    elif rune == "P":
-        drawPoison(surface, matrixIndex, cellSize, 20)
-    elif rune == "F":
-        drawFood(surface, matrixIndex, cellSize, 20)
-    else:
-        drawGnome(surface, matrixIndex, cellSize, rune)
-    # Add other runes here when needed
-
-# Draws a gnome, in a given direction and cell.
-def drawGnome(surface, matrixIndex, cellSize, direction):
-    pos = calculatePosFromIndex(matrixIndex, cellSize)
-    surface.blit(gnomeSheet[direction], [a + b for a, b in zip(pos, offsetVec[direction])])
-
-# Draws entire board, including background, lines and other sprites.
-def drawBoardFromMatrix(surface, m, grid, cellSize):
-    drawGrid(surface, grid, cellSize)
-    for x in range(len(m)):
-        for y in range(len(m[x])):
-            drawRune(surface, m[x][y], [x, y], cellSize)
+    # Draws entire board, including background, lines and other sprites.
+    def draw_board_from_matrix(self, matrix):
+        self.draw_grid()
+        for x in range(len(matrix)):
+            for y in range(len(matrix[x])):
+                self.draw_rune(matrix[x][y], [x, y])
+        pygame.display.update()
--- graphics_test.py
+++ graphics_test.py
@@ -4,21 +4,15 @@ import time
 import pygame
 
 class TestGraphics(unittest.TestCase):
-    def testSimpleGraphicsFromMatrix(self):
+    def test_simple_graphics_from_matrix(self):
         matrix = [ [0 for i in range(10)] for i in range(10)]
         matrix[3] = ["P" for i in range(10)]
 
         for i in range(10):
             matrix[i][4] = "F"
 
-        windowDimensions = [700, 700]
-        grid = [10, 10]
-        cellSize = [windowDimensions[0]/grid[0], windowDimensions[1]/grid[1]]
-        windowSurface = pygame.display.set_mode((windowDimensions[0], windowDimensions[1]), 0, 32)
-
-        pygame.display.update()
-        graphics.drawBoardFromMatrix(windowSurface, matrix, grid, cellSize)
-        pygame.display.update()
+        painter = graphics.Painter()
+        painter.draw_board_from_matrix(matrix)
         time.sleep(1)
 
     def testFoodPoisonClauseFromMatrix(self):
@@ -28,21 +22,14 @@ class TestGraphics(unittest.TestCase):
         for i in range(10):
             matrix[i][4] = "F"
 
-        windowDimensions = [700, 700]
-        grid = [10, 10]
-        cellSize = [windowDimensions[0]/grid[0], windowDimensions[1]/grid[1]]
-        windowSurface = pygame.display.set_mode((windowDimensions[0], windowDimensions[1]), 0, 32)
-
         matrix[5][7] = "D"
         matrix[1][4] = "U"
         matrix[0][9] = "R"
         matrix[3][0] = "L"
 
-        graphics.drawBoardFromMatrix(windowSurface, matrix, grid, cellSize)
-        pygame.display.update()
+        painter = graphics.Painter()
+        painter.draw_board_from_matrix(matrix)
         time.sleep(1)
-
-
 
 if __name__ == '__main__':
     unittest.main()
