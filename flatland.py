b8f79630629c8c64c9e4647f3a0162ada0604ecc
Olav Markussen <olavbm@samfundet.no>
2016-04-02 19:49:11 +0200

    Adds index_board(). Takes care of indexing with a given position.

--- flatland.py
+++ flatland.py
@@ -17,6 +17,7 @@ sense_pad = {
         }
 
 board = [[]]
+
 # Create a matrix holding a board of the given size.  Also fills the board with
 # food and poison in accordance to Flatland.
 def create_board(size, f, p):
@@ -43,11 +44,15 @@ def init_boards(num_boards, size, f, p):
 
     return boards
 
+def index_board(board, pos):
+    l = len(board)
+    return board[pos[0] % l][pos[1] % l]
+
 # Get the position of the player from a board.
 def get_pos(board):
     for x in range(len(board)):
         for y in range(len(board[x])):
-            if board[x][y] in pad:
+            if index_board(board, (x, y)) in pad:
                 return [x, y]
 
 # For manipulating the board according to an action given by the ann. Gives
@@ -56,14 +61,13 @@ def modify_on_action(board, action):
     n = len(board)
     old_pos = get_pos(board)
 
-    action = rulf_to_ruld(action, board[old_pos[0]][old_pos[1]])
+    action = rulf_to_ruld(action, index_board(board, old_pos))
     if not action:
         return (board, '0')
 
     offset = pad[action]
     new_pos = ((old_pos[0] + offset[0]) % n, (old_pos[1] + offset[1]) % n)
-    rune = board[new_pos[0]][new_pos[1]]
-
+    rune = index_board(board, new_pos)
 
     board[new_pos[0]][new_pos[1]] = action
     board[old_pos[0]][old_pos[1]] = 0
@@ -74,13 +78,14 @@ def modify_on_action(board, action):
 def sensor_cells(board):
     n = len(board)
     pos = get_pos(board)
-    direction = board[pos[0]][pos[1]]
+    direction = index_board(board, pos)
     cells = sense_pad[direction]
     sensor_cells = []
     for cell in cells:
-        sensor_cells.append(board
-                [(pos[0] + cell[0]) % n]
-                [(pos[1] + cell[1]) % n])
+        sensor_cells.append(
+                index_board(board, (pos[0] + cell[0],
+                                   pos[1] + cell[1]))
+                )
     return sensor_cells
 
 def rulf_to_ruld(rulf, init_ruld):
