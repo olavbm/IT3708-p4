7c0412e00f539449f7e6f7f341dfd2e2678e152c
Olav Markussen <olavbm@samfundet.no>
2016-04-07 16:21:54 +0200

    Add 3-layer recurrent network.
    Has support for talking to and from flatland.

--- neuron.py
+++ neuron.py
@@ -1,27 +1,33 @@
 import numpy as np
-import pprint
 
 iter_limit = 100
-input_count = 6
-hidden_count = 0
-output_count = 3
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def runes_to_stim(runes):
+    food = [rune == 'F' for rune in runes]
+    poison = [rune == 'P' for rune in runes]
+    return np.array(food + poison, dtype='float64')
+
+def output_to_rulf(output):
+    if max(output) < 0.3:
+        return 'U'
+
+    return 'RLF'[np.where(output == max(output))[0]]
 
 class Neural_net(object):
     def __init__ (self, weights):
         self.weights = weights
-        self.hidden_count = weights.shape[0]
-        self.syn = np.ones(self.hidden_count)
-
-    def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
+        self.syn = np.zeros(weights.shape[0])
 
     def act_on_input(self, inputs):
+        inputs = runes_to_stim(inputs)
         syn = self.syn
         dot = np.dot
         cat = np.concatenate
-        sigmoid = self.sigmoid
-        syn = sigmoid(dot(inputs, self.weights))
-        for i in range(len(self.weights)):
-            syn = sigmoid(dot(self.weights[i], syn))
+        weights = self.weights
+        for x in range(iter_limit):
+            syn = sigmoid(dot(weights, cat((inputs, syn))))
         self.syn = syn
-        return syn
+        return output_to_rulf(syn[:3])
