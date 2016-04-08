4cf4e6dc0c34317bb65ee8ae370452746d15ea8f
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2016-04-08 17:51:19 +0200

    Add visualization

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from multiprocessing import Process
+
+import ea
 import visualization
+
+from communication import Shared
+
+def coproc(target, *args):
+    Process(target=target, args=args).start()
 
 def main():
 
     # There can only be one ubernisse.
 
+    current_generation = Shared()
+    coproc(visualization.loop, current_generation)
+
     for generation in ea.run():
         print(generation)
+        current_generation.put(generation)
 
 if __name__ == '__main__':
     main()
