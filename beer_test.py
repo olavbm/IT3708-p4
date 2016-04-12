801255495e97c1e3747a15c78a63e7aa129cb33f
Olav Markussen <olavbm@samfundet.no>
2016-04-12 14:23:36 +0200

    Add new tests for beer-logic

--- beer_test.py
+++ beer_test.py
@@ -1,46 +1,47 @@
-import unittest
-import flatland
+import beer
+import copy
 import pprint
 import random
-import copy
+import time
+import unittest
 
-class TestFlatland(unittest.TestCase):
-    def test_generate_single_board(self):
-        board = flatland.create_board(10, 0.1,0.2)
-        assert board is not None
+class TestBeer(unittest.TestCase):
+    def test_spawn_agent(self):
+        board = [ ["N" for y in range(30)] for x in range(15)]
+        board = beer.spawn_agent(board)
 
-    def test_generate_num_boards(self):
-        boards = flatland.init_boards(3, 3, 0.1, 0.1)
-        for board in boards:
-            assert board is not None
+        counter = 0
+        for x in board:
+            for y in x:
+                if y == "A":
+                    counter += 1
+        assert counter == 5
+    def test_spawn_object(self):
+        board = [ ["N" for y in range(30)] for x in range(15)]
+        board = beer.spawn_object(board)
+
+        counter = 0
+        for x in board:
+            for y in x:
+                if y in "SB":
+                    counter += 1
+        assert 0 < counter < 7
+
+    def test_fall(self):
+        board = beer.create_board([30, 15])
+        beer.fall(board)
+        assert "S" in board[1] or "B" in board[1]
 
 
-    def test_get_pos(self):
-        board = flatland.create_board(5,0.1,0.2)
-        board[1][3] = "U"
-        pos = flatland.get_pos(board)
-        self.assertEqual(pos, [1,3])
 
-    def test_modify_board(self):
-        board = flatland.create_board(5, 0.4, 0.1)
-        board[1][1] = "U"
-        b = copy.deepcopy(board)
-        board, rune = flatland.modify_on_action(board, "L")
-        self.assertNotEqual(b, board)
 
-    def test_modify_board_eat_rune(self):
-        board = flatland.create_board(5,0.3,0.1)
-        board[1][1] = "U"
-        board[1][0] = "F"
-        board, rune = flatland.modify_on_action(board, "F")
-        self.assertEqual(rune, "F")
 
-    def test_sensor_cells(self):
-        board = flatland.create_board(5, 0.3,0.2)
-        board[1][3] = "U"
-        sensor_cells = flatland.sensor_cells(board)
-        for cell in sensor_cells:
-            assert cell is not None
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
