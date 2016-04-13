13eed71f0d9e352d31df4d876e8aa33e2993b10a
Olav Markussen <olavbm@samfundet.no>
2016-04-13 13:00:51 +0200

    Remove option for number of boards.

--- main.py
+++ main.py
@@ -11,24 +11,21 @@ def coproc(target, *args):
     Process(target=target, args=args).start()
 
 def main():
-    run_type = 'static'
-    num_boards = 1
+    run_type = 'standard'
 
-    options, args = getopt.getopt(sys.argv[1:],"t:n:")
+    options, args = getopt.getopt(sys.argv[1:],"t:")
     for opt, arg in options:
         if opt == '-t':
             run_type = arg
-        elif opt == '-n':
-            num_boards = int(arg)
         else:
-            print("Usage: %s -t run_type -n num_board" % sys.argv[0])
+            print("Usage: %s -t run_type" % sys.argv[0])
 
     # There can only be one ubernisse.
 
     current_generation = Shared()
     coproc(visualization.loop, current_generation)
 
-    for generation in ea.run(run_type, num_boards):
+    for generation in ea.run(run_type):
         print(generation)
         current_generation.put(generation)
 
