26b21cecc6d33d59d0f354451cc5f9d1ef0e09f8
Olav Markussen <olavbm@samfundet.no>
2016-04-13 11:42:45 +0200

    Change some tests in graphics_test.py

--- graphics_test.py
+++ graphics_test.py
@@ -8,8 +8,8 @@ class TestGraphics(unittest.TestCase):
         matrix = [ ["N" for i in range(10)] for i in range(10)]
         matrix[3] = ["P" for i in range(10)]
 
-        matrix[0][0] = "S"
-        matrix[0][1] = "A"
+        matrix[8][0] = "S"
+        matrix[8][9] = "A"
 
         painter = graphics.Painter()
         painter.draw_board_from_matrix(matrix)
@@ -49,12 +49,12 @@ class TestGraphics(unittest.TestCase):
         matrix = [ ["N" for i in range(30)] for i in range(15)]
         print len(matrix)
 
-        matrix[8][4] = "B"
-        matrix[9][4] = "B"
-        matrix[10][4] = "B"
-        matrix[11][4] = "B"
-        matrix[12][4] = "B"
-        matrix[12][4] = "B"
+        matrix[0][0] = "B"
+        matrix[1][0] = "B"
+        matrix[2][0] = "B"
+        matrix[3][0] = "B"
+        matrix[4][0] = "B"
+        matrix[5][0] = "B"
 
         matrix[14][14] = "A"
         matrix[13][14] = "A"
@@ -63,7 +63,7 @@ class TestGraphics(unittest.TestCase):
 
         painter = graphics.Painter()
         painter.draw_board_from_matrix(matrix)
-        time.sleep(2)
+        time.sleep(5)
 
 if __name__ == '__main__':
     unittest.main()
