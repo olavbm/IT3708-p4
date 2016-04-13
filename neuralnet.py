1ffb5ce46837afeaca0a91a4f2a52d6f18f52fff
Olav Markussen <olavbm@samfundet.no>
2016-04-13 19:02:11 +0200

    Add random initialization to neural net

--- neuralnet.py
+++ neuralnet.py
@@ -1,17 +1,41 @@
+from collections import namedtuple
+import pprint
 import numpy as np
+
+input_count = 6
+hidden_count = 2
+output_count = 2
 
 dot = np.dot
 cat = np.concatenate
 bias = np.array([1.0])
 
+Net_parameters = namedtuple('Net_parameters', 'weight gain mass')
+
+def random():
+    weight = 0
+    gain = np.random.uniform(1.0, 5.0, [hidden_count + output_count])
+    mass = np.random.uniform(1.0, 2.0, [hidden_count + output_count])
+    bias = np.random.uniform(-10.0, 0.0, [hidden_count + output_count, 1])
+    dims = (hidden_count + output_count, input_count + hidden_count + output_count)
+    weight = np.random.uniform(-5.0, 5.0, [dims[0], dims[1] -1])
+    weight = cat([weight, bias], axis=1)
+    weight[0:2, 8:10] = 0
+    weight[2:5, 0:5] = 0
+    parameters = Net_parameters(weight, gain, mass)
+
+    return parameters
+
 def make_CTRNN(output_count=2):
     class CTRNN(object):
-        def __init__ (self, weight, gain, mass):
-            self.weight = weight
-            self.gain = gain
-            self.mass = mass
-            self.state = np.zeros(weight.shape[0])
-            self.output = np.zeros(weight.shape[0])
+        def __init__ (self, parameters=None):
+            if parameters is None:
+                parameters = random()
+            self.weight = parameters.weight
+            self.gain = parameters.weight
+            self.mass = parameters.mass
+            self.state = np.zeros(self.weight.shape[0])
+            self.output = np.zeros(self.weight.shape[0])
 
         def act_on_input(self, inputs):
 
@@ -26,3 +50,4 @@ def make_CTRNN(output_count=2):
             return self.outputs[:output_count]
 
     return CTRNN
+random()
